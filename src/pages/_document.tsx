024665626d1dbb054094171e96bd9272b05d7dc7
skyheat <6574274+skyheat@users.noreply.github.com>
2023-09-18 18:27:51 +0800

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
  return (
   <Html>
    <Head />
+   <link rel="icon" href="/favicon.ico" sizes="any" />
+   <link rel="manifest" href="/site.webmanifest" />
+   <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
    <body>
     <Main />
     <NextScript />

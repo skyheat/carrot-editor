c46160825af0efc31c5475776a5e45f32a3c06fb
skyheat <6574274+skyheat@users.noreply.github.com>
2024-04-30 17:39:45 +0800

    Changed center align button stroke width

--- src/components/WidthControlDivider/index.tsx
+++ src/components/WidthControlDivider/index.tsx
@@ -23,7 +23,7 @@ const WidthControlDivider = ({
      viewBox="0 0 24 24"
      fill="none"
      stroke="currentColor"
-     strokeWidth="1"
+     strokeWidth="2"
      strokeLinecap="round"
      strokeLinejoin="round"
     >
